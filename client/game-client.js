f8dbcf6816c8d79982554fcc44a57e4362cdd2f6
Mormert <github@mormert.com>
2019-08-02 17:22:04 +0200

    Added game canvas

--- client/game-client.js
+++ client/game-client.js
@@ -1,1 +1,33 @@
-console.log("Game is running!");
+console.log("GAME RUNNING!");
+
+var config = {
+    type: Phaser.AUTO,
+    width: 600,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create,
+        update: update
+    }
+};
+
+var game = new Phaser.Game(config);
+
+function preload ()
+{
+
+
+
+}
+
+function create ()
+{
+
+
+}
+
+function update ()
+{
+
+    
+}
